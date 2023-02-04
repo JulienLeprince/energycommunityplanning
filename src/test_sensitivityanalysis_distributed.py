dc74bcf0e1be3ef74b3a6414a37a2b4f1fcea2ef
AmosSchledorn <amos.schledorn@icloud.com>
2023-02-04 09:09:35 -0800

    typo fix 'houly' to 'hourly' in test_sensitivityanalysis_distributed

--- src/test_sensitivityanalysis_distributed.py
+++ src/test_sensitivityanalysis_distributed.py
@@ -26,7 +26,7 @@ folder = arg_timestep + '_' + arg_folder_name + str(date.today()) + '/'
 if arg_timestep == '15mins':
     path_in = '../data/in/15mins/'
 if arg_timestep == 'hourly':
-    path_in = '../data/in/houly/'
+    path_in = '../data/in/hourly/'
 path_out = '../data/out/' + folder
 # path_out = r'C:\Users\20190285\surfdrive\05_Data\054_inout\0548_ECP\out/' + folder
 path_src = ''
